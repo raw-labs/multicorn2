0e3d3d7f38d69ea417098c43dd7137832eb3e8db
Benjamin Gaidioz <ben@raw-labs.com>
2025-02-13 09:25:01 +0100

    Edit EXPLAIN prefix to DAS

--- src/multicorn.c
+++ src/multicorn.c
@@ -592,7 +592,7 @@ multicornExplainForeignScan(ForeignScanState *node, ExplainState *es)
     Py_INCREF(p_iterable);
     while((p_item = PyIter_Next(p_iterable))){
         p_str = PyObject_Str(p_item);
-        ExplainPropertyText("Multicorn", PyString_AsString(p_str), es);
+        ExplainPropertyText("DAS", PyString_AsString(p_str), es);
         Py_DECREF(p_str);
     }
     Py_DECREF(p_iterable);
